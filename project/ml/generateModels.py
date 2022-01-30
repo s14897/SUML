9e763d9f0cb5fab5b061e488b041d84ad6c49311
Michal <s14897@pjwstk.edu.pl>
2022-01-30 10:24:53 +0100

    update komentarzy generateModels

--- project/ml/generateModels.py
+++ project/ml/generateModels.py
@@ -22,11 +22,13 @@ import os
 TRAINING_DATA = 'ml/data/zbior_treningowy.csv'
 
 def prepare_Data(directory, b_size_1, epochs1, activation1, activation2, output_dims1, b_size2, epochs2, activation3, activation4, activation5, activation6, output_dims2, output_dims3, output_dims4):
+    #przygotowanie danych i wczytanie zbioru, wczytujemy nasz plik ktory bylby na "serwerze"
     maindirectory = TRAINING_DATA
     datasetmain = pd.read_csv(maindirectory, sep=';')
     print(f'file: {directory}')
     datasettest =  pd.read_csv(directory, sep=';')
 
+    #czyscimy niepotrzebne kolumny
     dataset=datasetmain.drop(columns="NAZWISKO")
     datasettest=datasettest.drop(columns="NAZWISKO")
 
@@ -50,26 +52,29 @@ def prepare_Data(directory, b_size_1, epochs1, activation1, activation2, output_
     X_train = sc.fit_transform(X_train)
     X_test = sc.transform(X_test)
 
+    #funkcja do generowania auc roc
     def auc_roc(y_true, y_pred):
-        # any tensorflow metric
+        
         value, update_op = tf.contrib.metrics.streaming_auc(y_pred, y_true)
 
-        # find all variables created for this metric
+        # wszystkie zmienne dla auc roc
         metric_vars = [i for i in tf.local_variables() if 'auc_roc' in i.name.split('/')[1]]
 
-        # Add metric variables to GLOBAL_VARIABLES collection.
-        # They will be initialized for new session.
+        
         for v in metric_vars:
             tf.add_to_collection(tf.GraphKeys.GLOBAL_VARIABLES, v)
 
-        # force to update metric values
+        # wymuszenie aktualizacji
         with tf.control_dependencies([update_op]):
             value = tf.identity(value)
             return value
 
+    #ustawienie callbacka wplywajacego na prezentowanie sie wartosci w konsoli
     my_callbacks = [EarlyStopping(monitor='val_accuracy', patience=300, verbose=1, mode='max')]
 
     def create_first_model(X_train1, y_train1, X_test, y_test, b_size_1, epoch1, activation1, activation2, output_dims1):
+        #ustawienie modelu wedlug wyboru uzytkownika (model prostszy), ustawienie jadra, warstw wejsciowych wyjsciowych, funkcji aktywacji
+        #oraz trenowanie sieci oraz ustawienie zbioru walidacyjnego
         classifier1 = Sequential()
         classifier1.add(Dense( output_dims1, kernel_initializer = 'uniform', activation = activation1, input_dim = 11))
         classifier1.add(Dense( 3, kernel_initializer = 'uniform', activation = activation2))
@@ -85,6 +90,7 @@ def prepare_Data(directory, b_size_1, epochs1, activation1, activation2, output_
         return classifier1, history, history_test, pred_prob
 
     def create_third_model(X_train, y_train, X_test, y_test, b_size_1, epoch1, activation1, activation2, activation3, activation4, output_dims1, output_dims2, output_dims3):
+        #ustawienie modelu wedlug wyboru uzytkownika (model trudniejszy)
         classifier3 = Sequential()
         classifier3.add(Dense( output_dims1, kernel_initializer = 'uniform', activation = activation1, input_dim = 11))
         classifier3.add(Dense( output_dims2, kernel_initializer = 'uniform', activation = activation2))
@@ -103,6 +109,7 @@ def prepare_Data(directory, b_size_1, epochs1, activation1, activation2, output_
 
 
     def generate_plot_model_one(X_test, y_test, X_train, y_train, classifier1, history, history_test, X_prob):
+        #przygotowanie zmiennych do generowania plotow i ustawienia wykresow dla pierwszego modelu
         final1_test=classifier1.evaluate(X_test,y_test)
         final1=classifier1.evaluate(X_train,y_train)
 
@@ -141,6 +148,7 @@ def prepare_Data(directory, b_size_1, epochs1, activation1, activation2, output_
 
         
     def generate_plot_model_two(X_test, y_test, X_train, y_train, classifier2, history, history_test, X_prob):
+        #przygotowanie zmiennych do generowania plotow i ustawienia wykresow dla drugiego modelu
         final1_test=classifier2.evaluate(X_test,y_test)
         final1=classifier2.evaluate(X_train,y_train)
 
@@ -177,14 +185,10 @@ def prepare_Data(directory, b_size_1, epochs1, activation1, activation2, output_
 
 
     def final_score_data_table(b_size_1, epochs1, b_size_2, epochs2, classifier1, classifier2, X_test1, X_train1, y_train1,  y_test1 ):
+        #obliczanie wynikow finalnych
         cls1 = KerasClassifier(build_fn = classifier1, batch_size = b_size_1, epochs = epochs1)
-        # accuracies1 = cross_val_score(estimator = cls1, X = X_train1, y = y_train1, cv = 5, n_jobs = 1)
-
-        #Średnia precyzja dopasowania modelu
-        # mean_1 = accuracies1.mean()
         
-        #Odchylenie standardowe
-        # variance_1 = accuracies1.std()
+        
         from sklearn import metrics
         y_pred1 = classifier1.predict(X_test1)
         #Wartosc funkcji starty
@@ -196,13 +200,7 @@ def prepare_Data(directory, b_size_1, epochs1, activation1, activation2, output_
         final1=classifier1.evaluate(X_train1,y_train1)
         
         cls2 = KerasClassifier(build_fn = classifier2, batch_size = b_size_2, epochs = epochs2)
-        #accuracies2 = cross_val_score(estimator = cls2, X = X_train1, y = y_train1, cv = 5, n_jobs = 1, 
-        #                      verbose=1)
-        #Średnia precyzja dopasowania modelu
-        #mean_2 = accuracies2.mean()
-        
-        #Odchylenie standardowe
-        #variance_2 = accuracies2.std()
+       
         
         #dokladnosc dopasowania zbioru testowego
         final2_test=classifier2.evaluate(X_test,y_test)
@@ -215,7 +213,7 @@ def prepare_Data(directory, b_size_1, epochs1, activation1, activation2, output_
         score2 = metrics.log_loss(y_test, y_pred2)
 
         return final1_test, final2_test, final1, final2, score1, score2
-
+    #tutaj wywolanie wszystkich funkcji potrzebnych do generowania modeli oraz zwrocenie wynikow
     classifier1, history, history_test, pred_prob= create_first_model(X_train, y_train, X_test, y_test, b_size_1, epochs1, activation1, activation2, output_dims1)
     generate_plot_model_one(X_test, y_test, X_train, y_train, classifier1, history, history_test, pred_prob)
     classifier2, history2, history_test2, pred_prob2=create_third_model(X_train, y_train, X_test, y_test, b_size2, epochs2, activation3, activation4, activation5, activation6, output_dims2, output_dims3, output_dims4)
@@ -223,6 +221,7 @@ def prepare_Data(directory, b_size_1, epochs1, activation1, activation2, output_
     final1_test, final2_test, final1, final2, score1, score2 =final_score_data_table(b_size_1, b_size2, epochs1, epochs2, classifier1, classifier2, X_test, X_train, y_train,  y_test )    
     return final1_test, final2_test, final1, final2, score1, score2
 
+#przykladowe wywolanie
 # try:
 #print(prepare_Data('/home/adam/projects/python/suml/pro/ml/data/zbior_testowy.csv', 8,5,'relu', 'relu', 3, 3,5,'relu', 'relu', 'elu', 'softmax', 14, 14, 13))
 # except:
